10024ce4cb7508c5744706983c145d6eca884940
Shubhamsharma585 <shubhamsharma585@gmail.com>
2021-04-18 13:35:56 +0530

    Updates

---
+++ precache-manifest.40ec575c9ad3b353fe8c32285133c5a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63894ae9e9f775530c8fa94b56f80304",
+    "url": "./index.html"
+  },
+  {
+    "revision": "09a9770a943bf0179750",
+    "url": "./static/css/main.50c1325d.chunk.css"
+  },
+  {
+    "revision": "8160c87fed272194fe99",
+    "url": "./static/js/2.66aa9ab5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.66aa9ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a9770a943bf0179750",
+    "url": "./static/js/main.9607af20.chunk.js"
+  },
+  {
+    "revision": "9c003353d9c66a78de9a",
+    "url": "./static/js/runtime-main.d84addaa.js"
+  }
+]);
